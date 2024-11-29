64f2d4bc8e02da621fce415695cae2bc554fd870
Sebin <1219sebin@gmail.com>
2024-11-29 17:29:06 +0900

    kr.py의 함수만 쓰면 됩니다

--- krRVprompt.py
+++ krRVprompt.py
@@ -27,73 +27,84 @@ Objective와의 연결고리 강화, 직접적인 기여도 명확화, 핵심 �
 {prefix_example}
 """
 
-#"guideline" : "우리맛 연구팀의 레시피 개발을 통해 고객이 요리에 대한 접근성을 얼마나 높였는가? 우리맛 연구팀의 콘텐츠를 통해 고객의 요리에 대한 흥미와 참여를 얼마나 높였는가? 우리맛 연구팀의 노력을 통해 고객이 하루에 한 끼는 요리를 하고 싶게 만드는 데 얼마나 성공했는가?",
+# "guideline" : "우리맛 연구팀의 레시피 개발을 통해 고객이 요리에 대한 접근성을 얼마나 높였는가? 우리맛 연구팀의 콘텐츠를 통해 고객의 요리에 대한 흥미와 참여를 얼마나 높였는가? 우리맛 연구팀의 노력을 통해 고객이 하루에 한 끼는 요리를 하고 싶게 만드는 데 얼마나 성공했는가?",
 
 krRV_examples = [
-  {
-    "guideline" : "추가 필요",
-    "input_sentence" : "일상 요리들의 조리 과정을 50% 줄일 수 있는 제품+요리법을 연구, 개발한다.",
-    "upper_objective" : "고객이 하루에 한 끼는 요리를 하고 싶게 한다.",
-    "ex_description": "조리를 쉽고 빠르게 할 수 있는 것은 고객이 하루에 한끼 정도는 요리를 할 수 있게 만드는 요소입니다. 현재대비 50% 정도로 요리 과정이 간소화되어야 하는데, 현재가 어느정도인지 나타나지 않아서 50%로 줄어드는 것을 측정하기 어렵다. 조리과정 50% 감소라는 것은 결과이나, 조리과정이 현재 어느정도에서 얼마나 달라지는지로 표현되어야 더 좋은 결과다",
-    "revision" : "일상 요리 조리 평균 시간을 50분에서 25분으로 줄인다.",
-    "description" : "구체적인 판단의 근거를 제시함",
-  }
+    {
+        "guideline": "추가 필요",
+        "input_sentence": "일상 요리들의 조리 과정을 50% 줄일 수 있는 제품+요리법을 연구, 개발한다.",
+        "upper_objective": "고객이 하루에 한 끼는 요리를 하고 싶게 한다.",
+        "EV_description": "조리를 쉽고 빠르게 할 수 있는 것은 고객이 하루에 한끼 정도는 요리를 할 수 있게 만드는 요소입니다. 현재대비 50% 정도로 요리 과정이 간소화되어야 하는데, 현재가 어느정도인지 나타나지 않아서 50%로 줄어드는 것을 측정하기 어렵다. 조리과정 50% 감소라는 것은 결과이나, 조리과정이 현재 어느정도에서 얼마나 달라지는지로 표현되어야 더 좋은 결과다",
+        "revision": "일상 요리 조리 평균 시간을 50분에서 25분으로 줄인다.",
+        "description": "구체적인 판단의 근거를 제시함",
+    }
 ]
 
 krRV_example_prompt = PromptTemplate(
-  input_variables = ["guideline", "input_sentence", "upper_objective", "ex_description", "predict_value_score", "description", "revision"],
-  template = """
+    input_variables=[
+        "guideline",
+        "input_sentence",
+        "upper_objective",
+        "EV_description",
+        "description",
+        "revision",
+    ],
+    template="""
+  <입력>
   "guideline" : {guideline}
   "input_sentence": {input_sentence}
   "upper_objective": {upper_objective}
-  "ex_description": {ex_description}
+  "EV_description": {EV_description}
+  
   <출력>
   "revision" : {revision}
   "description" : {description}
-  """
+  """,
 )
 
 krRV_suffix = """
 <입력>
-"guideline": {guideline_krRV}
+"guideline": {guideline}
 "input_sentence": {input_sentence}
 "upper_objective": {upper_objective}
-"ex_description": {ex_description}
-"revision":
+"EV_description": {EV_description}
+
+<출력>
 "description":
+"revision":
 
 출력 형식은 key가 description과 revision 2개인 json 형식입니다. json이라는 문구나 백틱 같은 특수문자는 사용하지 마십시오.
 """
 
 krRV_examples_1 = [
-  {
-    "guideline" : "추가 필요",
-    "input_sentence" : "일상 요리들의 조리 과정을 50% 줄일 수 있는 제품+요리법을 연구, 개발한다.",
-    "upper_objective" : "고객이 하루에 한 끼는 요리를 하고 싶게 한다.",
-    "ex_description": "조리를 쉽고 빠르게 할 수 있는 것은 고객이 하루에 한끼 정도는 요리를 할 수 있게 만드는 요소입니다.",
-    "description": "추가 필요",
-    "revision" : "추가 필요",
-  }
+    {
+        "guideline": "추가 필요",
+        "input_sentence": "일상 요리들의 조리 과정을 50% 줄일 수 있는 제품+요리법을 연구, 개발한다.",
+        "upper_objective": "고객이 하루에 한 끼는 요리를 하고 싶게 한다.",
+        "EV_description": "조리를 쉽고 빠르게 할 수 있는 것은 고객이 하루에 한끼 정도는 요리를 할 수 있게 만드는 요소입니다.",
+        "description": "추가 필요",
+        "revision": "추가 필요",
+    }
 ]
 
 krRV_examples_2 = [
-  {
-    "guideline" : "추가 필요",
-    "input_sentence" : "일상 요리들의 조리 과정을 50% 줄일 수 있는 제품+요리법을 연구, 개발한다.",
-    "upper_objective" : "고객이 하루에 한 끼는 요리를 하고 싶게 한다.",
-    "ex_description": "조리를 쉽고 빠르게 할 수 있는 것은 고객이 하루에 한끼 정도는 요리를 할 수 있게 만드는 요소입니다.",
-    "description": "추가 필요",
-    "revision" : "추가 필요",
-  }
+    {
+        "guideline": "추가 필요",
+        "input_sentence": "일상 요리들의 조리 과정을 50% 줄일 수 있는 제품+요리법을 연구, 개발한다.",
+        "upper_objective": "고객이 하루에 한 끼는 요리를 하고 싶게 한다.",
+        "EV_description": "조리를 쉽고 빠르게 할 수 있는 것은 고객이 하루에 한끼 정도는 요리를 할 수 있게 만드는 요소입니다.",
+        "description": "추가 필요",
+        "revision": "추가 필요",
+    }
 ]
 
 krRV_examples_3 = [
-  {
-    "guideline" : "추가 필요",
-    "input_sentence" : "일상 요리들의 조리 과정을 50% 줄일 수 있는 제품+요리법을 연구, 개발한다.",
-    "upper_objective" : "고객이 하루에 한 끼는 요리를 하고 싶게 한다.",
-    "ex_description": "조리를 쉽고 빠르게 할 수 있는 것은 고객이 하루에 한끼 정도는 요리를 할 수 있게 만드는 요소입니다.",
-    "description": "추가 필요",
-    "revision" : "추가 필요",
-  }
+    {
+        "guideline": "추가 필요",
+        "input_sentence": "일상 요리들의 조리 과정을 50% 줄일 수 있는 제품+요리법을 연구, 개발한다.",
+        "upper_objective": "고객이 하루에 한 끼는 요리를 하고 싶게 한다.",
+        "EV_description": "조리를 쉽고 빠르게 할 수 있는 것은 고객이 하루에 한끼 정도는 요리를 할 수 있게 만드는 요소입니다.",
+        "description": "추가 필요",
+        "revision": "추가 필요",
+    }
 ]
